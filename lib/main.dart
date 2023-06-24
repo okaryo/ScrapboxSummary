17ccac5c91cc697525ad964ee7ed380cff032086
okaryo <okaryotr@gmail.com>
2023-06-24 17:03:36 +0900

    :sparkles: add form template

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,144 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('ScrapboxSummary'),
+          shape: const Border(
+            bottom: BorderSide(
+              color: Color(0xFFD1D1D1),
+              width: 1.0,
+            ),
+          ),
+        ),
+        body: const SummaryForm(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SummaryForm extends StatefulWidget {
+  const SummaryForm({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SummaryForm> createState() => _SummaryFormState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _SummaryFormState extends State<SummaryForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _apiKey = '';
+  String _model = 'gpt-4';
+  String _scrapboxProjectName = 'okaryo';
+  String _scrapboxAuthToken = '';
+  String _scrapboxPages = '';
+  String _prompt = '''
+以下の開発日誌の内容を週報としてまとめてください。''';
+  String _output = '';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 675),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  initialValue: _apiKey,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'OpenAI API Key',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  initialValue: _model,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'モデル名',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  initialValue: _scrapboxProjectName,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Scrapboxのプロジェクト名',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  initialValue: _scrapboxAuthToken,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Scrapboxの認証情報(プライベートプロジェクトの場合)',
+                    hintText: 'Cookie内のconnect.sidの値',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  initialValue: _scrapboxPages,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '要約したいページ(改行区切りで複数指定可)',
+                  ),
+                  onChanged: (value) {},
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  initialValue: _prompt,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '要約用プロンプト(プロンプトの下にページの内容が追加される)',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => _summarize(),
+                    child: const Text('要約する'),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  initialValue: _output,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '出力結果',
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _summarize() async {
+    final cookies = 'connect.sid=$_scrapboxAuthToken';
+    final res = await http.get(
+      Uri.parse('https://scrapbox.io/api/pages/okaryo/ScrapboxSummary/text'),
+      headers: {
+        'Cookie': cookies,
+      },
     );
   }
 }
